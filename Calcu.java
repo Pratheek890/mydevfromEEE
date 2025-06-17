431fac032ed6b4865442f9afbab06362e2fddc16
Pratheek890 <113978228+Pratheek890@users.noreply.github.com>
2025-06-17 21:42:03 +0530

    Add files via upload

---
+++ Calcu.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+public class Calcu {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int ans= 0;
+        while(true){
+        System.out.println("Enter the operator ");
+         char op=input.next().trim().charAt(0);
+        if ( op == '+' || op == '-' || op == '*' || op == '/' || op =='%') {
+            System.out.println("Enter the  numbers: ");
+            int a = input.nextInt();
+            int b = input.nextInt();
+            if(op == '+'){
+                ans = a + b;
+            }
+            if(op == '-'){
+                ans = a - b;
+            }
+            if(op == '*'){
+                ans = a * b;
+            }
+            if(op == '/'){
+                if (a!= 0 && b!=0){
+                    ans = a/b;
+                }
+             }    
+            if(op == '%'){
+                ans = a % b;       
+            }
+        
+            } else if (op == 'x' || op == 'X'){
+                System.out.println("Exiting the calculator.");
+                break;
+        }    
+            else {
+                System.out.println("Invalid operator");
+           
+        }
+    
+
+        System.out.println("the answer of " + ans);
+}
+}
+
+}
+
+// thank you so much
